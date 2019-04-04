1b36a89e7b39f8a2f4da4a0e6231b78756339159
calvin-fisher <1797159+calvin-fisher@users.noreply.github.com>
2019-04-04 10:59:18 -0500

    Initial project state

---
+++ DesignEnder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AutoDesignEnder
+{
+    public class AutoDesignEnder : ModMeta
+    {
+        public override string Name => "Auto Design Ender";
+
+        public override void ConstructOptionsScreen(RectTransform parent, bool inGame)
+        {
+        }
+    }
+}
---
+++ DesignEnderBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace AutoDesignEnder
+{
+    public class DesignEnderBehaviour : ModBehaviour
+    {
+        public override void OnActivate()
+        {
+            Console.WriteLine("Activating");
+
+            timer.Elapsed += UpdateProjects;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        public override void OnDeactivate()
+        {
+        }
+
+        private readonly Timer timer = new Timer(250);
+
+        private void UpdateProjects(object sender, ElapsedEventArgs e)
+        {
+            Debug.WriteLine("Updating projects");
+
+            if (GameSettings.Instance == null
+                || GameSettings.Instance.MyCompany == null
+                || GameSettings.Instance.MyCompany.WorkItems == null)
+            {
+                return;
+            }
+
+            var workItems = GameSettings.Instance.MyCompany.WorkItems;
+            var designDocuments = workItems.OfType<DesignDocument>().ToList();
+
+            foreach (var designDocument in designDocuments)
+            {
+                if (designDocument.HasFinished && !designDocument.Done)
+                {
+                    designDocument.PromoteAction();
+                }
+            }
+        }
+
+    }
+}
